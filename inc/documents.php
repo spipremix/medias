046d376f04554a40e4c2d93e955aae3954780d10
Cerdic <cedric@yterium.com>
2011-03-02 11:01:10 +0000

    s'abstenir d'une verificatiion sur la globale browser_barre pour inserer le js ondlbclick sur les raccourcis documents

--- inc/documents.php
+++ inc/documents.php
@@ -188,9 +188,7 @@ function affiche_raccourci_doc($doc, $id, $align) {
 
 	if ($align) {
 		$pipe = "|$align";
-
-		if ($GLOBALS['browser_barre'])
-			$onclick = "\nondblclick=\"barre_inserer('\\x3C$doc$id$pipe&gt;', $('textarea[name=texte]')[0]);\"\ntitle=\"". str_replace('&amp;', '&', entites_html(_T('double_clic_inserer_doc')))."\"";
+		$onclick = "\nondblclick=\"barre_inserer('\\x3C$doc$id$pipe&gt;', $('textarea[name=texte]')[0]);\"\ntitle=\"". str_replace('&amp;', '&', entites_html(_T('double_clic_inserer_doc')))."\"";
 	} else {
 		$align='center';
 	}
