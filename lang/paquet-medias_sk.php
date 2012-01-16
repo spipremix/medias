b4eb025435f14cd0a8e00c1b6b18a9cd2848fd9d
salvatore <salvatore@rezo.net>
2012-01-16 09:53:35 +0000

    langues (paquet-medias)

--- lang/paquet-medias_sk.php
+++ lang/paquet-medias_sk.php
@@ -8,8 +8,8 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// M
-	'medias_description' => 'Riadenie médií SPIPu', # MODIF
-	'medias_slogan' => 'Riadenie médií v SPIPe' # MODIF
+	'medias_description' => 'Správa multimédií v SPIPe',
+	'medias_slogan' => 'Správa multimédií v SPIPe'
 );
 
 ?>
