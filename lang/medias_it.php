2c2701182a4e706118808997c2f1bbabe1953fa5
elvis.deltedesco <elvis.deltedesco@phante.com>
2012-04-25 06:37:15 +0000

    langues (medias)

--- lang/medias_it.php
+++ lang/medias_it.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org/tradlang_module/medias?lang_cible=it
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,181 +8,183 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'aucun_audio' => 'Aucune bande son', # NEW
-	'aucun_document' => 'Aucun document', # NEW
-	'aucun_file' => 'Aucun document', # NEW
-	'aucun_image' => 'Aucune image', # NEW
-	'aucun_video' => 'Aucune s&eacute;quence', # NEW
-	'aucune_utilisation' => 'Ce document n\'est pas utilis&eacute;', # NEW
-	'aucune_vignette' => 'Aucune vignette', # NEW
+	'aucun_audio' => 'Nessun suono',
+	'aucun_document' => 'Nessun documento',
+	'aucun_file' => 'Nessun documento',
+	'aucun_image' => 'Nessuna immagine',
+	'aucun_video' => 'Nessun video',
+	'aucune_utilisation' => 'Questo documento non è utilizzato',
+	'aucune_vignette' => 'Nessuna miniatura',
 
 	// B
 	'bouton_ajouter_document' => 'Aggiungi un documento',
 	'bouton_ajouter_image' => 'Aggiungi un\'immagine',
 	'bouton_ajouter_image_document' => 'Aggiungi un\'immagine o un documento',
-	'bouton_attacher_document' => 'Attacher', # NEW
-	'bouton_copier_local' => 'Copier dans le site', # NEW
-	'bouton_download_depuis' => 'depuis&nbsp;:', # NEW
-	'bouton_download_local' => 'mon ordinateur', # NEW
-	'bouton_download_par_ftp' => 'le serveur', # NEW
-	'bouton_download_par_mediatheque' => 'la mediath&egrave;que', # NEW
-	'bouton_download_sur_le_web' => 'internet', # NEW
-	'bouton_enlever_document' => 'Detacher', # NEW
-	'bouton_enlever_supprimer_document' => 'Supprimer', # NEW
-	'bouton_enlever_supprimer_document_confirmation' => 'Etes-vous s&ucirc;r de vouloir supprimer le document ?', # NEW
-	'bouton_modifier_document' => 'Modifier', # NEW
-	'bouton_parcourir' => 'Parcourir...', # NEW
-	'bouton_refresh_document' => 'Rafraichir le document', # NEW
-	'bouton_remplacer_document' => 'Remplacer ce document', # NEW
-	'bouton_remplacer_image' => 'Remplacer cette image', # NEW
-	'bouton_remplacer_image_document' => 'Remplacer par une image ou un document', # NEW
-	'bouton_supprimer' => 'Supprimer', # NEW
-	'bouton_supprimer_tous_orphelins' => 'Supprimer tous les orphelins', # NEW
-	'bouton_supprimer_tous_orphelins_confirmation' => 'Etes-vous s&ucirc;r de vouloir supprimer @nb@ orphelins ?', # NEW
-	'bouton_upload' => 'T&eacute;l&eacute;charger', # NEW
+	'bouton_attacher_document' => 'Allega',
+	'bouton_copier_local' => 'Copia sul sito',
+	'bouton_download_depuis' => 'da:',
+	'bouton_download_local' => 'mio computer',
+	'bouton_download_par_ftp' => 'il server',
+	'bouton_download_par_mediatheque' => 'media library',
+	'bouton_download_sur_le_web' => 'internet',
+	'bouton_enlever_document' => 'Scollega',
+	'bouton_enlever_supprimer_document' => 'Cancella',
+	'bouton_enlever_supprimer_document_confirmation' => 'Sei sicuro di voler cancellare il documento?',
+	'bouton_modifier_document' => 'Modifica',
+	'bouton_parcourir' => 'Sfoglia',
+	'bouton_refresh_document' => 'Aggiorna il documento',
+	'bouton_remplacer_document' => 'Sostituisci il documento',
+	'bouton_remplacer_image' => 'Sostituisci l\'immagine',
+	'bouton_remplacer_image_document' => 'Sostituisci con un\'immagine o un documento',
+	'bouton_supprimer' => 'Cancella',
+	'bouton_supprimer_tous_orphelins' => 'Cancella tutti i non utilizzati',
+	'bouton_supprimer_tous_orphelins_confirmation' => 'Vuoi veramente cancellare @nb@ documenti non utilizzati?',
+	'bouton_upload' => 'Carica',
 
 	// D
-	'des_audios' => '@nb@ bandes sons', # NEW
-	'des_documents' => '@nb@ documents', # NEW
-	'des_files' => '@nb@ documents', # NEW
-	'des_images' => '@nb@ images', # NEW
-	'des_utilisations' => '@nb@ utilisations', # NEW
-	'des_videos' => '@nb@ s&eacute;quences', # NEW
-	'descriptif' => 'Description', # NEW
-	'document_attache_succes' => 'Le document a bien &eacute;t&eacute; ajout&eacute;', # NEW
-	'document_copie_locale_succes' => 'Le fichier a bien &eacute;t&eacute; copi&eacute; dans le site', # NEW
-	'document_installe_succes' => 'Le fichier a bien &eacute;t&eacute; charg&eacute;', # NEW
-	'document_vu' => 'Ins&eacute;r&eacute; dans le texte', # NEW
-	'documents' => 'Mediath&egrave;que', # NEW
-	'documents_brises' => 'Bris&eacute;s', # NEW
-	'documents_distants' => 'Distants', # NEW
-	'documents_locaux' => 'Locaux', # NEW
-	'documents_non_publies' => 'Non publi&eacute;s', # NEW
-	'documents_orphelins' => 'Inutilis&eacute;s', # NEW
-	'documents_publies' => 'Publi&eacute;s', # NEW
-	'documents_tous' => 'Tous', # NEW
+	'des_audios' => '@nb@ suoni',
+	'des_documents' => '@nb@ documenti',
+	'des_files' => '@nb@ documenti',
+	'des_images' => '@nb@ immagini',
+	'des_utilisations' => '@nb@ utilizzazioni',
+	'des_videos' => '@nb@ video',
+	'descriptif' => 'Descrizione',
+	'document_attache_succes' => 'Il documento è stato aggiunto con successo',
+	'document_copie_locale_succes' => 'Il documento è stato copiato con successo nel sito',
+	'document_installe_succes' => 'Il file è stato caricato con successo',
+	'document_vu' => 'Inserisci nel testo',
+	'documents' => 'Media library',
+	'documents_brises' => 'Corrotto',
+	'documents_distants' => 'Remoto',
+	'documents_locaux' => 'Locale',
+	'documents_non_publies' => 'Non pubblicato',
+	'documents_orphelins' => 'Non utilizzato',
+	'documents_publies' => 'Pubblicato',
+	'documents_tous' => 'Tutto',
 	'double_clic_inserer_doc' => 'Doppio clic qui per inserire la scorciatoia nel testo',
 
 	// E
 	'entree_dimensions' => 'Dimensioni:',
 	'entree_titre_document' => 'Titolo del documento:',
 	'entree_titre_image' => 'Titolo dell\'immagine:',
-	'erreur_aucun_document' => 'Ce document n\'existe pas dans la mediath&egrave;que', # NEW
-	'erreur_aucun_fichier' => 'Aucun fichier n\'a &eacute;t&eacute; trouv&eacute;', # NEW
-	'erreur_chemin_distant' => 'Le fichier distant @nom@ n\'a pas pu &ecirc;tre trouv&eacute;', # NEW
-	'erreur_chemin_ftp' => 'Le fichier indiqu&eacute; n\'a pas &eacute;t&eacute; trouv&eacute; sur le serveur', # NEW
-	'erreur_copie_fichier' => 'Impossible de copier le fichier @nom@', # NEW
-	'erreur_dossier_tmp_manquant' => 'Un dossier temporaire est manquant pour télécharger les fichiers', # NEW
-	'erreur_ecriture_fichier' => 'Erreur lors de l\'écriture du fichier sur le disque', # NEW
-	'erreur_format_fichier_image' => 'Le format de @nom@ ne convient pas pour une image', # NEW
-	'erreur_indiquez_un_fichier' => 'Indiquez un fichier !', # NEW
-	'erreur_insertion_document_base' => 'Impossible d\'enregistrer le document @fichier@ en base de donn&eacute;es', # NEW
-	'erreur_suppression_vignette' => 'Erreur lors de la suppression de la vignette', # NEW
-	'erreur_upload_type_interdit' => 'Le telechargement des fichiers du type de @nom@ n\'est pas autoris&eacute;', # NEW
-	'erreur_upload_vignette' => 'Erreur lors du chargement de la vignette @nom@', # NEW
-	'erreurs' => '@nb@ erreurs', # NEW
-	'erreurs_voir' => 'Voir les @nb@ erreurs', # NEW
+	'erreur_aucun_document' => 'Il documento non esiste nella media library',
+	'erreur_aucun_fichier' => 'Nessun file è stato trovato',
+	'erreur_chemin_distant' => 'Il file remoto @nom@ non può essere trovato',
+	'erreur_chemin_ftp' => 'Il file indicato non è stato trovato nel server',
+	'erreur_copie_fichier' => 'Impossibile copiare il file @nom@',
+	'erreur_dossier_tmp_manquant' => 'La directory temporanea per scaricare il file è mancante',
+	'erreur_ecriture_fichier' => 'Errore scrivendo il file sul disco',
+	'erreur_format_fichier_image' => 'Il formato di @nom@ non è assimilabile a un\'immagine',
+	'erreur_indiquez_un_fichier' => 'Specifica un file!',
+	'erreur_insertion_document_base' => 'Impossibile salvare il documento @fichier@ nel database',
+	'erreur_suppression_vignette' => 'Errore durante la cancellazione della miniatura',
+	'erreur_upload_type_interdit' => 'Il caricamento di file del tipo @nom@ non è consentito',
+	'erreur_upload_vignette' => 'Errore caricando la miniatura  @nom@',
+	'erreurs' => '@nb@ errori',
+	'erreurs_voir' => 'Controlla gli @nb@ errori',
 
 	// F
-	'fichier_distant' => 'Fichier distant', # NEW
-	'fichier_manquant' => 'Fichier manquant', # NEW
-	'fichier_modifie' => 'Le fichier a &eacute;t&eacute; modifi&eacute;. Cliquez sur enregistrer.', # NEW
-	'format_date_incorrect' => 'La date ou l\'heure sont incorrectes', # NEW
+	'fichier_distant' => 'File remoto',
+	'fichier_manquant' => 'File mancante',
+	'fichier_modifie' => 'Il file è stato modificato. Clicca su pulsante Salva',
+	'format_date_incorrect' => 'La data o l\'ora sono sbagliate',
 
 	// I
-	'icone_creer_document' => 'Ajouter un document', # NEW
-	'id' => 'ID', # NEW
+	'icone_creer_document' => 'Aggiungi un documento',
+	'id' => 'ID',
 	'image_tourner_180' => 'Ruota 180°',
 	'image_tourner_droite' => 'Ruota 90° a destra',
 	'image_tourner_gauche' => 'Ruota 90° a sinistra',
-	'info_doc_max_poids' => 'Les documents doivent obligatoirement faire moins de @maxi@ (ce fichier fait @actuel@).', # NEW
+	'info_doc_max_poids' => 'Il documento deve necessariamente essere più piccolo di @maxi@ (il file è attualmente @actuel@).',
 	'info_document' => 'Documento',
 	'info_document_indisponible' => 'Questo documento non è disponibile',
 	'info_documents' => 'Documenti',
-	'info_gauche_numero_document' => 'DOCUMENT NUM&Eacute;RO', # NEW
-	'info_hauteur' => 'Hauteur', # NEW
-	'info_heure' => 'Heure', # NEW
-	'info_illustrations' => 'Illustrations', # NEW
-	'info_image_max_poids' => 'Les images doivent obligatoirement faire moins de @maxi@ (ce fichier fait @actuel@).', # NEW
+	'info_gauche_numero_document' => 'DOCUMENTO NUMERO',
+	'info_hauteur' => 'Altezza',
+	'info_heure' => 'Ora',
+	'info_illustrations' => 'Illustrazioni',
+	'info_image_max_poids' => 'L\'immagine deve essere necessariamente grande meno di @maxi@ (il file è @actuel@).',
 	'info_inclusion_directe' => 'Inclusione diretta:',
 	'info_inclusion_vignette' => 'Inclusione dell\'immagine:',
 	'info_installer_tous_documents' => 'Installare tutti i documenti',
-	'info_largeur' => 'Largeur', # NEW
+	'info_largeur' => 'Larghezza',
 	'info_logo_max_taille' => 'I logo devono obbligatoriamente avere una dimensione inferiore a @maxi@ (questo file è di @actuel@).',
-	'info_modifier_document' => 'Modifier le document', # NEW
+	'info_modifier_document' => 'Modifica il documento',
 	'info_portfolio' => 'Portfolio',
 	'info_referencer_doc_distant' => 'Mettere in repertorio un documento su Internet:',
-	'info_statut_document' => 'Ce document est&nbsp;:', # NEW
+	'info_statut_document' => 'Il documento è:',
 	'info_telecharger' => 'Upload dal tuo computer:',
-	'infos' => 'Infos techniques', # NEW
+	'infos' => 'Informazioni tecniche',
 	'item_autoriser_selectionner_date_en_ligne' => 'Permetti di modificare la data di ogni documento',
 	'item_non_autoriser_selectionner_date_en_ligne' => 'La data dei documenti è quella in cui sono stati aggiunti al sito.',
 
 	// L
-	'label_activer_document_objets' => 'Activer les documents pour les contenus&nbsp;:', # NEW
-	'label_apercu' => 'Aper&ccedil;u', # NEW
-	'label_caracteristiques' => 'Caract&eacute;ristiques', # NEW
-	'label_credits' => 'Cr&eacute;dits', # NEW
-	'label_fichier' => 'Fichier', # NEW
-	'label_fichier_vignette' => 'Vignette', # NEW
-	'label_parents' => 'Ce document est li&eacute; &agrave;', # NEW
-	'label_refdoc_joindre' => 'Document num&eacute;ro', # NEW
-	'lien_tout_enlever' => 'Tout enlever', # NEW
-	'logo' => 'Logo', # NEW
+	'label_activer_document_objets' => 'Attiva il caricamento di documento per il contenuto:',
+	'label_apercu' => 'Anteprima',
+	'label_caracteristiques' => 'Specifiche',
+	'label_credits' => 'Crediti',
+	'label_fichier' => 'File',
+	'label_fichier_vignette' => 'Miniature',
+	'label_parents' => 'Il documento è collegato a',
+	'label_refdoc_joindre' => 'Documento numero',
+	'lien_tout_enlever' => 'Rimuovi tutto',
+	'logo' => 'Logo',
 
 	// M
-	'media_audio' => 'Bandes sons', # NEW
-	'media_file' => 'Autres', # NEW
-	'media_image' => 'Images', # NEW
-	'media_video' => 'S&eacute;quences', # NEW
+	'media_audio' => 'Suono',
+	'media_file' => 'Altro',
+	'media_image' => 'Immagini',
+	'media_video' => 'Video',
+	'miniature_automatique_active' => 'SPIP genererà automaticamente le miniature dell\'immagine',
 
 	// N
-	'nb_documents_installe_succes' => '@nb@ fichiers charg&eacute;s avec succ&egrave;s', # NEW
+	'nb_documents_attache_succes' => '@nb@ documenti sono stati aggiunti',
+	'nb_documents_installe_succes' => '@nb@ file sono stati caricati con successo',
 
 	// O
-	'objet_document' => 'Document', # NEW
-	'objet_documents' => 'Documents', # NEW
+	'objet_document' => 'Documento',
+	'objet_documents' => 'Documenti',
 
 	// P
-	'par_date' => 'Date', # NEW
-	'par_hauteur' => 'Hauteur', # NEW
-	'par_id' => 'ID', # NEW
-	'par_largeur' => 'Largeur', # NEW
-	'par_taille' => 'Poids', # NEW
-	'par_titre' => 'Titre', # NEW
+	'par_date' => 'Data',
+	'par_hauteur' => 'Altezza',
+	'par_id' => 'ID',
+	'par_largeur' => 'Larghezza',
+	'par_taille' => 'Peso',
+	'par_titre' => 'Titolo',
 
 	// T
-	'texte_documents_joints' => 'Puoi autorizzare l\'aggiunta di documenti (file di testo, fogli elettronici, immagini, file multimediali, ecc.) agli articoli e/o alle rubriche. Questi file possono poi essere richiamati nel testo dell\'articolo, o visualizzati separatamente.',
-	'texte_documents_joints_2' => 'Questa impostazione non impedisce l\'inserimento diretto di immagini negli articoli.',
+	'texte_documents_joints' => 'Puoi abilitare l\'interfaccia per l\'aggiunta di documenti (file di testo, fogli elettronici, immagini, file multimediali, ecc.) agli articoli, rubriche e altro. Questi file possono poi essere richiamati nel testo dell\'articolo, o visualizzati separatamente.',
+	'texte_documents_joints_2' => 'Questa impostazione non impedisce il caricamento diretto di immagini nell\'articolo ne l\'inclusione di documenti nel testo del contenuto.',
 	'titre_documents_joints' => 'Documenti allegati',
-	'titre_page_documents_edit' => 'Modifier le document : @titre@', # NEW
-	'tous_les_medias' => 'Tous les medias', # NEW
+	'titre_page_documents_edit' => 'Modifica il documento: @titre@',
+	'tous_les_medias' => 'Tutti i media',
 	'tout_dossier_upload' => 'Tutta la cartella @upload@',
-	'tout_voir' => 'Tout voir', # NEW
+	'tout_voir' => 'Mostra tutto',
 
 	// U
-	'un_audio' => '1 bande son', # NEW
-	'un_document' => '1 document', # NEW
-	'un_file' => '1 document', # NEW
-	'un_image' => '1 image', # NEW
-	'un_video' => '1 s&eacute;quence', # NEW
-	'une_utilisation' => '1 utilisation', # NEW
+	'un_audio' => '1 suono',
+	'un_document' => '1 documento',
+	'un_file' => '1 documento',
+	'un_image' => '1 immagine',
+	'un_video' => '1 video',
+	'une_utilisation' => '1 utilizzazione',
 	'upload_fichier_zip' => 'File ZIP',
 	'upload_fichier_zip_texte' => 'Il file proposto è un file ZIP.',
 	'upload_fichier_zip_texte2' => 'Questo file può essere:',
-	'upload_info_mode_document' => 'D&eacute;poser dans le portfolio', # NEW
-	'upload_info_mode_image' => 'Retirer du portfolio', # NEW
+	'upload_info_mode_document' => 'Invia al portfolio',
+	'upload_info_mode_image' => 'Rimuovi dal portfolio',
 	'upload_limit' => 'Questo file è troppo grande per il server; la dimensione massima consentita per gli <i>upload</i> è di @max@.',
-	'upload_zip_conserver' => 'Conserva l\'archivio al termine dell\'estrazione',
+	'upload_zip_conserver' => 'Conserva il file archivio dopo l\'estrazione del contenuto',
 	'upload_zip_decompacter' => 'decompresso e ogni elemento installato nel sito. I file che saranno installati nel sito sono:',
-	'upload_zip_mode_document' => 'D&eacute;poser toutes les images dans le portfolio', # NEW
+	'upload_zip_mode_document' => 'Inserisci tutte le immagini nel portfolio',
 	'upload_zip_telquel' => 'installato così com\'è in quanto archivio compresso Zip;',
 	'upload_zip_titrer' => 'Dare i titoli in base al nome dei file',
 
 	// V
-	'verifier_documents_brises' => 'V&eacute;rifier les fichiers manquants', # NEW
-	'verifier_documents_inutilises' => 'V&eacute;rifier les liens des documents', # NEW
-	'vignette_supprimee' => 'La vignette a &eacute;t&eacute; supprim&eacute;e' # NEW
+	'verifier_documents_brises' => 'Controlla i file mancanti',
+	'verifier_documents_inutilises' => 'Controlla i link ai documenti',
+	'vignette_supprimee' => 'La miniatura è stata cancellata'
 );
 
 ?>
