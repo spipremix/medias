c0fdf8d77a02e9c2945c1f6d38ef565695429784
Matthieu Marcillaud <marcimat@rezo.net>
2012-06-20 12:37:40 +0000

    Notices PHP en moins : le serveur n'est pas toujours connu, qu'on se le dise

--- medias_pipelines.php
+++ medias_pipelines.php
@@ -80,6 +80,8 @@ function medias_configurer_liste_metas($config){
 
 
 function medias_post_edition($flux){
+	// le serveur n'est pas toujours la
+	$serveur = (isset($flux['args']['serveur']) ? $flux['args']['serveur'] : '');
 	// si on ajoute un document, mettre son statut a jour
 	if($flux['args']['action']=='ajouter_document'){
 		include_spip('action/editer_document');
@@ -92,9 +94,9 @@ function medias_post_edition($flux){
 		// verifier d'abord les doublons !
 		include_spip('inc/autoriser');
 		if (autoriser('autoassocierdocument',$type,$flux['args']['id_objet'])){
-			$table_objet = isset($flux['args']['table_objet'])?$flux['args']['table_objet']:table_objet($flux['args']['table'],$flux['args']['serveur']);
+			$table_objet = isset($flux['args']['table_objet'])?$flux['args']['table_objet']:table_objet($flux['args']['table'],$serveur);
 			$marquer_doublons_doc = charger_fonction('marquer_doublons_doc','inc');
-			$marquer_doublons_doc($flux['data'],$flux['args']['id_objet'],$type,id_table_objet($type, $flux['args']['serveur']),$table_objet,$flux['args']['table'], '', $flux['args']['serveur']);
+			$marquer_doublons_doc($flux['data'],$flux['args']['id_objet'],$type,id_table_objet($type, $serveur),$table_objet,$flux['args']['table'], '', $serveur);
 		}
 
 		if($flux['args']['action']=='instituer' OR isset($flux['data']['statut'])){
@@ -111,7 +113,7 @@ function medias_post_edition($flux){
 		if ($flux['args']['table']!=='spip_documents'){
 			// verifier les doublons !
 			$marquer_doublons_doc = charger_fonction('marquer_doublons_doc','inc');
-			$marquer_doublons_doc($flux['data'],$flux['args']['id_objet'],$flux['args']['type'],id_table_objet($flux['args']['type'], $flux['args']['serveur']),$flux['args']['table_objet'],$flux['args']['spip_table_objet'], '', $flux['args']['serveur']);
+			$marquer_doublons_doc($flux['data'],$flux['args']['id_objet'],$flux['args']['type'],id_table_objet($flux['args']['type'], $serveur),$flux['args']['table_objet'],$flux['args']['spip_table_objet'], '', $serveur);
 		}
 	}
 	return $flux;
