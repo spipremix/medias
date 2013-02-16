957065f43b26f3d82e5f63de00c11ba90476a045
Matthieu Marcillaud <marcimat@rezo.net>
2013-02-16 10:41:28 +0000

    Depuis http://core.spip.org/projects/spip/repository/revisions/17004 et r43738 il existe un pipeline detecter_fond_par_defaut qu'utilise normalement la médiathèque, mais… comme le pipeline n'était pas déclaré, on ne passait jamais dans cette fonction.

--- medias_pipelines.php
+++ medias_pipelines.php
@@ -10,6 +10,12 @@
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
 \***************************************************************************/
 
+/**
+ * Utilisations de pipelines 
+ *
+ * @package SPIP\Medias\Pipelines
+**/
+
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
