080b33dff45c88a167c4fabd0410799d4de020b8
tcharlss <tcharlss@hotmail.fr>
2014-11-28 13:56:15 +0000

    Prise en compte des fichiers .md (markdown, la syntaxe de l'an 2000). Jusqu'à présent,  ils étaient détectés comme des .m3u (liste de lecture mp3).

--- medias_administrations.php
+++ medias_administrations.php
@@ -149,6 +149,10 @@ function medias_upgrade($nom_meta_base_version,$version_cible){
 		// ajout de json
 		array('creer_base_types_doc')
 	);
+	$maj['1.2.6'] = array(
+		// ajout de md (markdown)
+		array('creer_base_types_doc')
+	);
 	include_spip('base/upgrade');
 	include_spip('base/medias');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
