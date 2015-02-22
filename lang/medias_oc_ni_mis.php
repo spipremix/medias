8ffa773f14a36222fabfa5c82efb31a2ca39ce70
sylvain <sylvain@oliver-fr.org>
2015-02-22 04:33:31 +0000

    [Salvatore] [source:_core_/plugins/medias/lang/ medias] Export depuis http://trad.spip.net de la langue oc_ni_mis

---
+++ lang/medias_oc_ni_mis.php
@@ -0,0 +1,153 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/medias?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aucun_audio' => 'Mìnga benda sounoura',
+	'aucun_document' => 'Mìnga doucumen',
+	'aucun_file' => 'Mìnga doucumen',
+	'aucun_image' => 'Mìnga image',
+	'aucun_video' => 'Mìnga sequènsa',
+	'aucune_utilisation' => 'Aquéu doucumen es pas utilisat',
+	'aucune_vignette' => 'Mìnga vigneta',
+
+	// B
+	'bouton_ajouter_document' => 'Ajustà un doucumen',
+	'bouton_ajouter_image' => 'Ajustà una image',
+	'bouton_ajouter_image_document' => 'Ajustà una image o un doucumen',
+	'bouton_attacher_document' => 'Estacà',
+	'bouton_copier_local' => 'Coupià en lou sit',
+	'bouton_download_depuis' => 'despì :',
+	'bouton_download_local' => 'lou miéu ourdinatour',
+	'bouton_download_par_ftp' => 'lou servidour',
+	'bouton_download_par_mediatheque' => 'la mediatèca',
+	'bouton_download_sur_le_web' => 'internet',
+	'bouton_enlever_document' => 'Destacà',
+	'bouton_enlever_supprimer_document' => 'Suprimà',
+	'bouton_enlever_supprimer_document_confirmation' => 'Sias segur de voulé suprimà lou doucumen ?',
+	'bouton_modifier_document' => 'Moudificà',
+	'bouton_parcourir' => 'Escourre…',
+	'bouton_refresh_document' => 'Atualisà lou doucumen',
+	'bouton_remplacer_document' => 'Remplaçà aqueu doucumen',
+	'bouton_remplacer_image' => 'Remplaçà aqueula image',
+	'bouton_remplacer_image_document' => 'Remplaçà per una image o un doucumen',
+	'bouton_supprimer' => 'Suprimà',
+	'bouton_upload' => 'Televersà',
+
+	// D
+	'des_audios' => '@nb@ benda sounouri',
+	'des_documents' => '@nb@ doucumen',
+	'des_files' => '@nb@ doucumen',
+	'des_images' => '@nb@ image',
+	'des_utilisations' => '@nb@ utilisacioun',
+	'des_videos' => '@nb@ sequènsa',
+	'document_attache_succes' => 'Lou doucumen es ben estat ajustat',
+	'document_copie_locale_succes' => 'Lou fichié es ben estat coupiat en lou sit',
+	'document_installe_succes' => 'Lou fichié es ben estat cargat',
+	'document_vu' => 'Inserà en lou tèst',
+	'documents' => 'Mediatèca',
+	'documents_non_publies' => 'Noun publicat',
+	'documents_publies' => 'Publicat',
+	'documents_tous' => 'Toui',
+	'double_clic_inserer_doc' => 'Bespicàs da inserà aqueu escourcha en lou tèst',
+
+	// E
+	'entree_titre_document' => 'Titre dóu doucumen :',
+	'entree_titre_image' => 'Titre de l’image :',
+	'erreur_aucun_document' => 'Aqueu doucumen noun esista en la mediatèca',
+	'erreur_aucun_fichier' => 'Mìnga fichié es estat troubat',
+	'erreur_chemin_ftp' => 'Lou fichié endicat es pas estat troubat soubre lou servidour',
+	'erreur_copie_fichier' => 'Empoussible de coupià lou fichié @nom@',
+	'erreur_ecriture_fichier' => 'Errour en tant que lou fichié s’escrieuguèsse sus lou disc',
+	'erreur_indiquez_un_fichier' => 'Endicàs un fichié !',
+	'erreur_insertion_document_base' => 'Empoussible de registrà lou doucumen @fichier@ en basa de dounada',
+	'erreur_suppression_vignette' => 'Errour en tant que si suprimèsse la vigneta',
+	'erreurs' => '@nb@ errour',
+	'erreurs_voir' => 'Veire li @nb@ errour',
+
+	// F
+	'fichier_modifie' => 'Lou fichié es etat moudificat. Picàs soubre registrà.',
+
+	// I
+	'icone_creer_document' => 'Ajustà un doucumen',
+	'id' => 'ID',
+	'image_tourner_180' => 'Routacioun 180°',
+	'image_tourner_droite' => 'Routacioun 90° a drecha',
+	'image_tourner_gauche' => 'Routacioun 90° a seneca',
+	'info_document' => 'Doucumen',
+	'info_document_indisponible' => 'Aquéu doucumen es pas dispounible',
+	'info_documents' => 'Doucumen',
+	'info_gauche_numero_document' => 'DOUCUMEN NUMERÒ',
+	'info_hauteur' => 'Autessa',
+	'info_heure' => 'Oura',
+	'info_image_max_poids' => 'Li image devon oubligatoriamen faire mens de @maxi@ (aqueu fichié fa @actuel@).',
+	'info_installer_tous_documents' => 'Instalà toui lu doucumen',
+	'info_largeur' => 'Larguessa',
+	'info_logo_max_taille' => 'Lu logou devon oubligatoriamen faire mens de @maxi@ (aqueu fichié fa @actuel@).',
+	'info_modifier_document' => 'Moudificà lou doucumen',
+	'info_portfolio' => 'Pouòrtafoliò',
+	'info_sans_vignette' => 'Sensa vigneta',
+	'info_statut_document' => 'Aquéu doucumen es :',
+	'info_telecharger' => 'Telecargà dau vouòstre ourdinatour :',
+
+	// L
+	'label_fichier' => 'Fichié',
+	'label_fichier_vignette' => 'Vigneta',
+	'label_refdoc_joindre' => 'Doucumen numerò',
+	'logo' => 'Logou',
+
+	// M
+	'media_audio' => 'Banda sounouri',
+	'media_file' => 'Autre',
+	'media_image' => 'Image',
+	'media_video' => 'Sequènsa',
+
+	// N
+	'nb_documents_attache_succes' => '@nb@ doucumen soun ben estat ajustat',
+
+	// O
+	'objet_document' => 'Doucumen',
+	'objet_documents' => 'Doucumen',
+
+	// P
+	'par_date' => 'Data',
+	'par_hauteur' => 'Autessa',
+	'par_id' => 'ID',
+	'par_largeur' => 'Larguessa',
+	'par_titre' => 'Titre',
+
+	// T
+	'titre_documents_joints' => 'Doucumen jounch',
+	'titre_page_documents_edit' => 'Moudificà lou doucumen : @titre@',
+	'tous_les_medias' => 'Toui lu media',
+	'tout_dossier_upload' => 'Tout lou doussié @upload@',
+	'tout_voir' => 'Tout veire',
+
+	// U
+	'un_audio' => '1 banda sounoura',
+	'un_document' => '1 doucumen',
+	'un_file' => '1 doucumen',
+	'un_image' => '1 image',
+	'un_video' => '1 sequènsa',
+	'une_utilisation' => '1 utilisacioun',
+	'upload_fichier_zip' => 'Fichié ZIP',
+	'upload_fichier_zip_texte' => 'Lou fichié que proupausàs d’instalà es un fichié Zip.',
+	'upload_fichier_zip_texte2' => 'Aquéu fichié pòu estre :',
+	'upload_info_mode_document' => 'Depousà en lou pouòrtafoliò',
+	'upload_info_mode_image' => 'Restirà dóu pouòrtafoliò',
+	'upload_limit' => 'Aqueu fichié es tròu gros per lou servidour; la talha massimala autourisada en <i>upload</i> es de @max@.',
+	'upload_zip_decompacter' => 'descoumprimat e cada elemen que counten instalat sus lou sit. Lu fichié que seran pi instalat sus lou sit soun :',
+	'upload_zip_mode_document' => 'Depousà touti li image en lou pouòrtafoliò',
+	'upload_zip_telquel' => 'instalat tau quau, couma arquivi  coumprimat Zip ;',
+
+	// V
+	'verifier_documents_inutilises' => 'Verificà lu estac dei doucumen',
+	'vignette_supprimee' => 'La vigneta es estada suprimada'
+);
+
+?>
