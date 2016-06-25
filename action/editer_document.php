eebc106c86f1c42206edb7f8bac70fb6e67f1666
Cerdic <cedric@yterium.com>
2016-06-25 18:09:03 +0000

    plusieurs petits bugs sur les documents distants mal formates qui pourraient exister en base par des operations peu avouables, on teste systematiquement que le fichier ressemble bien a une URL avant de s'y fier

--- action/editer_document.php
+++ action/editer_document.php
@@ -141,6 +141,7 @@ function document_modifier($id_document, $set = null) {
 		and $champs['fichier']
 		and $ancien_fichier // on avait bien note le nom du fichier avant la modif
 		and $ancien_fichier !== $champs['fichier'] // et il a ete modifie
+		and !tester_url_absolue($ancien_fichier)
 		and @file_exists($f = get_spip_doc($ancien_fichier))
 	) {
 		spip_unlink($f);
