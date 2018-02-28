e8c4005aa8cd32090b2ea929c5c408f71ffe91ca
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-28 04:36:17 +0000

    [Salvatore] [source:_core_/plugins/medias/lang/ medias] Export depuis http://trad.spip.net de la langue br

--- lang/medias_br.php
+++ lang/medias_br.php
@@ -10,6 +10,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
+	'affichage_documents_en_cases' => 'Diskwel al logoù',
+	'affichage_documents_en_grand' => 'Diskwel bras',
+	'affichage_documents_en_liste_compacte' => 'Diskwel al listenn fetis',
 	'aucun_audio' => 'Roudenn-son ebet',
 	'aucun_document' => 'Teul ebet',
 	'aucun_file' => 'Teul ebet',
@@ -31,13 +34,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'bouton_download_sur_le_web' => 'kenrouedad',
 	'bouton_enlever_document' => 'Distagañ',
 	'bouton_enlever_supprimer_document' => 'Dilemel',
+	'bouton_enlever_supprimer_document_confirmation' => 'C’hoant hoc’h eus da zilemel an teul ?',
 	'bouton_modifier_document' => 'Kemmañ',
 	'bouton_parcourir' => 'Furchal...',
 	'bouton_refresh_document' => 'Adtresañ an teul',
 	'bouton_remplacer_document' => 'Erlec’hiañ an teul-mañ',
 	'bouton_remplacer_image' => 'Erlec’hiañ ar skeudenn-mañ',
+	'bouton_remplacer_image_document' => 'Erlec’hiañ gant ur skeudenn pe un teul',
 	'bouton_supprimer' => 'Dilemel',
 	'bouton_supprimer_tous_orphelins' => 'Dilemel an holl emzivadoù',
+	'bouton_supprimer_tous_orphelins_confirmation' => 'C’hoant hoc’h eus da zilemel @nb@ emzivadoù ?',
 	'bouton_upload' => 'Pellgargañ',
 
 	// D
@@ -49,6 +55,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'des_videos' => '@nb@ sekañsoù',
 	'descriptif' => 'Deskrivadur',
 	'details_document' => 'Munudoù',
+	'details_document_afficher_masquer' => 'Diskwel / masklañ munudoù an teul',
 	'document_attache_succes' => 'Ouzhpennet eo bet an teul',
 	'document_copie_locale_succes' => 'Eilet eo bet ar restr el lec’h',
 	'document_installe_succes' => 'Karget eo bet ar restr',
@@ -67,27 +74,84 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'entree_dimensions' => 'Ment :',
 	'entree_titre_document' => 'Titl an teul :',
 	'entree_titre_image' => 'Titl ar skeudenn :',
+	'erreur_aucun_document' => 'N’emañ ket an teul-mañ er mediaoueg',
 	'erreur_aucun_fichier' => 'N’eus kavet teul ebet',
 	'erreur_autorisation' => 'Fazi en aotre.',
+	'erreur_chemin_distant' => 'N’eo ket bet kavet ar restr pell @nom@ ',
+	'erreur_chemin_ftp' => 'N’eo ket bet kavet an teul merket er servijer',
+	'erreur_copie_fichier' => 'Ne c’haller ket eilañ an teul @nom@',
+	'erreur_deplacement_impossible' => 'Ne c’haller ket dilec’hiañ.',
+	'erreur_format_fichier_image' => 'N’eo ket mat furmad @nom@ evit ur skeudenn',
+	'erreur_indiquez_un_fichier' => 'Merkit un teul !',
+	'erreur_insertion_document_base' => 'Ne c’haller ket enrollañ an teul @fichier@ en diaz-titouroù',
 	'erreurs' => '@nb@ fazioù',
 	'erreurs_voir' => 'Gwelet ar @nb@ fazioù',
 
+	// F
+	'fichier_distant' => 'Restr pell',
+	'fichier_manquant' => 'Restr diank',
+	'format_date_incorrect' => 'N’int ket mat an deiziad hag an eur',
+
 	// I
+	'icone_creer_document' => 'Ouzhpennañ un teul',
+	'id' => 'ID',
 	'image_tourner_180' => 'Treiñ da 180 derez',
 	'image_tourner_droite' => 'Treiñ da 90 derez war an tu dehou',
 	'image_tourner_gauche' => 'Treiñ da 90 derez war an tu kleiz',
 	'info_document' => 'Teul',
 	'info_document_indisponible' => 'N’eo ket hegerz an teul-mañ',
 	'info_documents' => 'Teulioù',
+	'info_duree' => 'Padelezh',
+	'info_gauche_numero_document' => 'TEUL NIVERENN',
+	'info_hauteur' => 'Uhelder',
+	'info_heure' => 'Eur',
 	'info_inclusion_directe' => 'Enklozañ war-eeun:',
 	'info_inclusion_vignette' => 'Enklozañ ar skeudennig :',
 	'info_installer_tous_documents' => 'Staliañ an holl deulioù',
+	'info_largeur' => 'Ledander',
 	'info_logo_max_taille' => 'Bihanoc’h eget @maxi@ e rank al logoioù bezañ (@actuel@ eo ar restr-mañ).',
+	'info_mode' => 'Doare',
+	'info_modifier_document' => 'Kemmañ an teul',
 	'info_portfolio' => 'Portfolio',
 	'info_referencer_doc_distant' => 'Menegeriñ un teul war ar Genrouedad :',
+	'info_sans_vignette' => 'Hep skeudennig',
+	'info_statut_document' => 'An teul-mañ zo :',
 	'info_telecharger' => 'Pellgargañ adal hoc’h urzhiataer :',
+	'info_utilisation' => 'Implij(où)',
 	'item_autoriser_selectionner_date_en_ligne' => 'Aotren kemmañ deiziad embann pep restr',
 	'item_non_autoriser_selectionner_date_en_ligne' => 'Deiziad ar restroù a vo hini an deiziad embann war al lec’hienn',
+
+	// L
+	'label_apercu' => 'Alberz',
+	'label_caracteristiques' => 'Doareoù',
+	'label_credits' => 'Kredadoù',
+	'label_fichier' => 'Restr',
+	'label_fichier_vignette' => 'Skeudennig',
+	'label_refdoc_joindre' => 'Teul niverenn',
+	'lien_tout_enlever' => 'Lemel an holl',
+	'logo' => 'Logo',
+
+	// M
+	'media_audio' => 'Roudennoù-son',
+	'media_file' => 'All',
+	'media_image' => 'Skeudennoù',
+	'media_video' => 'Sekañsoù',
+
+	// N
+	'nb_documents_attache_succes' => '@nb@ ouzhpenet eo bet an teulioù',
+
+	// O
+	'objet_document' => 'Teul',
+	'objet_documents' => 'Teulioù',
+	'ordonner_ce_document' => 'Ordren an teul-mañ',
+
+	// P
+	'par_date' => 'Deiziad',
+	'par_hauteur' => 'Uhelder',
+	'par_id' => 'ID',
+	'par_largeur' => 'Ledander',
+	'par_taille' => 'Pouez',
+	'par_titre' => 'Titl',
 
 	// T
 	'texte_documents_joints' => 'Gallout a rit aotren ma vo ouzhpennet restroù (restroù bureveg, skeudennoù,
@@ -95,9 +159,18 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 Gallout a ra restroù-se bezañ menegeret er pennad, pe diskwelet a-ziforc’h.', # MODIF
 	'texte_documents_joints_2' => 'Ne vir ket ar reizhadenn-se a ensoc’hañ skeudennoù er pennad war-eeun.', # MODIF
 	'titre_documents_joints' => 'Teulioù stag',
+	'titre_page_documents_edit' => 'Kemmañ an teul : @titre@',
+	'tous_les_medias' => 'An holl mediaoù',
 	'tout_dossier_upload' => 'An teuliad @upload@ a-bezh',
+	'tout_voir' => 'Gwelet an holl',
 
 	// U
+	'un_audio' => '1 roudenn-son',
+	'un_document' => '1 teul',
+	'un_file' => '1 teul',
+	'un_image' => '1 skeudenn',
+	'un_video' => '1 sekañs',
+	'une_utilisation' => '1 implij',
 	'upload_fichier_zip' => 'Restr ZIP',
 	'upload_fichier_zip_texte' => 'Ur restr Zip eo ar restr a ginnigit staliañ.',
 	'upload_fichier_zip_texte2' => 'Gallout a ra bezañ :',
@@ -105,5 +178,8 @@ Gallout a ra restroù-se bezañ menegeret er pennad, pe diskwelet a-ziforc’h.'
 	'upload_zip_conserver' => 'Mirout an diell goude an diwaskadur', # MODIF
 	'upload_zip_decompacter' => 'diwasket, ha pep elfenn ennañ staliet el lec’hienn. Ar restroù staliet a vo staliet war al lec’hienn neuze a vo :',
 	'upload_zip_telquel' => 'staliet evel-henn, da lavaret eo restroù gwasket Zip ;',
-	'upload_zip_titrer' => 'Titlañ diouzh anv ar restroù'
+	'upload_zip_titrer' => 'Titlañ diouzh anv ar restroù',
+
+	// V
+	'vignette_supprimee' => 'Dilemet eo bet ar skeudennig'
 );
