b1d84a573cc21386b341f9c9dfddf6bd74aa7b8c
gpl <gpl@long-player.net>
2018-03-15 04:42:04 +0000

    [Salvatore] [source:_core_/plugins/medias/lang/ paquet-medias] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-medias_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-medias?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'medias_description' => 'Mastrumado de ĉiuspecaj dokumentoj de SPIP',
+	'medias_slogan' => 'Mastrumado de ĉiuspecaj dokumentoj en SPIP'
+);
