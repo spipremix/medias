bdafae5332f05c42fecff2eedce6d98f02d534ed
Cerdic <cedric@yterium.com>
2018-09-27 14:06:32 +0000

    Fix https://core.spip.net/issues/4115 : eviter une collision de nommage dans ieconfig (est-ce que ca a sa place ici, vraiment?)

--- medias_ieconfig.php
+++ medias_ieconfig.php
@@ -6,9 +6,9 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 // On déclare ici la config du core
 function medias_ieconfig_metas($table) {
-	$table['medias']['titre'] = _T('medias:titre_documents_joints');
-	$table['medias']['icone'] = 'document-16.png';
-	$table['medias']['metas_brutes'] = 'documents_objets,documents_date';
+	$table['medias_meta']['titre'] = _T('medias:titre_documents_joints');
+	$table['medias_meta']['icone'] = 'document-16.png';
+	$table['medias_meta']['metas_brutes'] = 'documents_objets,documents_date';
 
 	return $table;
 }
